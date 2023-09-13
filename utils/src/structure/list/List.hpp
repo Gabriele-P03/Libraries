b0042fa56259117d861a3a6c6eade245ec2886a3
Gabriele-P03 <pacegabriele@libero.it>
2023-09-13 20:50:45 +0200

    ArrayList developing over. Testing...

--- utils/src/structure/list/List.hpp
+++ utils/src/structure/list/List.hpp
@@ -1,10 +1,8 @@
 /**
  * @file
  * 
- * This is an interface
  * 
  * A list is like a Set, but it does allow null and dulicate elements both.
- * 
  * A list is an indexed super-type collection, it means that you can insert elements via an index 
  * 
  * @date 2023-07-12
@@ -18,6 +16,7 @@
 #include "../Collection.hpp"
 #include <jpl/exception/runtime/IndexOutOfBoundsException.hpp>
 #include <jpl/exception/runtime/NotFoundException.hpp>
+#include <jpl/exception/runtime/NullPointerException.hpp>
 
 namespace jpl{
 
@@ -60,7 +59,7 @@ namespace jpl{
                      * 
                      * @throw NullPointerException if list does not permit null elements
                     */
-                    virtual void addAll(unsigned long index, List<T>* &list) = 0;
+                    virtual void addAll(unsigned long index, List<T>* list) = 0;
                     /**
                     * @brief Insert into the structure all items contained into list at the end
                     * if the array list
@@ -69,7 +68,7 @@ namespace jpl{
                     *
                     * @throw NullPointerException if list does not permit null elements
                     */
-                    virtual void addAll(List<T>* &list) = 0;          
+                    virtual void addAll(List<T>* list) = 0;          
 
                     /**
                      * @return the element at the given index
@@ -83,12 +82,12 @@ namespace jpl{
                      * @return the index of the first occurrence of t. 
                      * If t is not found into the list, it returns max 
                     */
-                    virtual unsigned long firstOccurrence(T* t) const noexcept = 0;
+                    virtual unsigned long firstOccurrence(T t) const noexcept = 0;
                     /**
                      * @return the index of the last occurrence of t.
                      * If t is not found into the list, it returns max
                     */
-                    virtual unsigned long lastOccurrence(T* t) const noexcept = 0;
+                    virtual unsigned long lastOccurrence(T t) const noexcept = 0;
 
                     
                     /**
@@ -105,16 +104,17 @@ namespace jpl{
                      */
                     virtual void remove(T t) = 0;                    
                     /**
-                     * @brief remove all collection's items from this collection
+                     * @brief remove all list's items from this collection
                      * 
-                     * @param list collection of items to remove
+                     * @param list list of items to remove
                      * @throw NotFoundException if at least one item into list has not been found
                      */
-                    virtual void removeAll(const List<T>* list) = 0;
+                    virtual void removeAll(List<T>* list) = 0;
                     /**
                      * @brief Remove all elements which respect the given predicate
+                     * @param predicate
                     */
-                    virtual void removeAllIf(_functional::Predicate<T>* predicate) noexcept = 0;
+                    virtual void removeAllIf(_functional::Predicate<T> predicate) noexcept = 0;
 
                     /**
                      * Set t at the given index (which was already occuped)
@@ -124,7 +124,7 @@ namespace jpl{
                      * @throw IndexOutOfBounds if index is graeter than length()-1 or less than 0
                      * @throw NullPointerException if list does not permit null elements and t it is
                     */
-                    virtual T* set(unsigned long index, const T &t) = 0;
+                    virtual T set(unsigned long index, const T &t) = 0;
 
                     /**
                      * @param start index of the first element to copy

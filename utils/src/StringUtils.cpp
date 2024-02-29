3be9bf33540ad27a920cccea2ea2dccd9aaec3ba
Gabriele-P03 <pacegabriele@libero.it>
2024-02-29 20:54:05 +0100

    StringUtils split

---
+++ utils/src/StringUtils.cpp
@@ -0,0 +1,26 @@
+#include "StringUtils.hpp"
+
+std::vector<std::string>* jpl::_utils::_string::split(std::string src, std::regex regex, const unsigned long maxSize){
+
+    std::vector<std::string>* vec = new std::vector<std::string>;
+
+    auto word_begin = std::sregex_token_iterator(src.begin(), src.end(), regex, -1);
+    auto word_end = std::sregex_token_iterator();
+
+    unsigned long count = 0;
+    for(; word_begin != word_end; ++word_begin){
+
+        std::string match_string = *word_begin;
+        vec->push_back(match_string);
+        if(maxSize > 0){
+            if(++count > maxSize)
+                break;
+        }
+    }
+
+    return vec;
+}
+
+std::vector<std::string>* jpl::_utils::_string::split(std::string src, std::regex regex){
+    return jpl::_utils::_string::split(src, regex, std::string::npos);
+}
---
+++ utils/src/StringUtils.hpp
@@ -0,0 +1,35 @@
+/**
+ * 
+ * SPLIT FUNCTION
+ * 
+*/
+
+#ifndef STRING_UTILS_JPL
+#define STRING_UTILS_JPL
+
+#include <string>
+#include <vector>
+#include <regex>
+
+namespace jpl{
+    namespace _utils{
+        namespace _string{
+
+            /**
+             * @return Splitted src using regex
+             * @param src
+             * @param regex
+             * @param maxSize max amount of splitted string to return
+            */
+            std::vector<std::string>* split(std::string src, std::regex regex, const unsigned long maxSize);
+            /**
+             * @return Splitted src using regex
+             * @param src
+             * @param regex
+            */
+            std::vector<std::string>* split(std::string src, std::regex regex);
+        }
+    }
+}
+
+#endif

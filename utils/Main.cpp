075fb30b15f478747f96278ffce956e753489deb
Gabriele-P03 <pacegabriele@libero.it>
2024-03-08 19:38:40 +0100

    Main.cpp sync

--- utils/Main.cpp
+++ utils/Main.cpp
@@ -1,34 +1,29 @@
-#include "src/structure/list/LinkedList.hpp"
+#include "src/debug/Profiler.hpp"
+#include "src/StringUtils.hpp"
 
 using namespace jpl;
 using namespace _utils;
-using namespace _collections;
-using namespace _list;
+
+#include <iostream>
+#include <chrono>
+#include <thread>
 
 int main(){
+    //std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 
-    LinkedList<int> ll = LinkedList<int>();
+    try{
+        jpl::_utils::_profiler::Profiler profiler;
+        profiler.start(100);
+        for(int i = 0; i < 1000000000; i++){
+        }
+        profiler.end();
 
-    ll.add(0, 1);
-    ll.add(0, 2);
-    ll.add(0, 3);
-    ll.add(0, 4);
-    ll.add(0, 5);
-
-    LinkedList<int> ll1 = LinkedList<int>();
-    ll1.add(0, 6);
-    ll1.add(0, 7);
-    ll1.add(0, 8);
-    ll1.add(0, 9);
-    ll1.add(0, 10);
-
-    ll.addAll(&ll1);
-
-
-    std::cout<<ll<<std::endl<<ll1<<std::endl;  
-    ll.removeAll(&ll1);
-    ll1.compress();
-    std::cout<<std::endl<<ll<<std::endl<<ll1;
-
-    return 0;
+        const std::vector<const jpl::_utils::_profiler::SystemInfo*>* vec = profiler.getSystemInfoList();
+        for(int i = 0; i < vec->size(); i++){
+            std::cout<<*vec->at(i);
+        }
+    }catch(const jpl::_exception::RuntimeException* ex){
+        std::cout<<*ex;
+    }
+    //std::cout<<memInfo->totalMemory;
 }

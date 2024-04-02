88344b715bd9132e3363427d70c0a5af2289f2f6
Gabriele-P03 <pacegabriele@libero.it>
2024-04-02 18:54:04 +0200

    Rollback to doxygen

--- utils/Main.cpp
+++ utils/Main.cpp
@@ -1,39 +1,7 @@
 #define DEL_EFF_DS_JPL
 #include "src/structure/list/LinkedList.hpp"
-
 #include "src/structure/list/ArrayList.hpp"
-
-using namespace jpl;
-using namespace _utils;
-using namespace _collections;
-using namespace _list; 
-
-static int c = 0;
-
-class Bar{
-
-    public:
-        int x;
-        Bar(){
-            this->x = ++c;
-        }
-        ~Bar(){
-            std::cout<<"Deleting "<<x<<"...";
-        }
-
-        friend std::ostream& operator<<(std::ostream &os, const Bar &bar){
-            os<<bar.x;
-            return os;
-        }
-};
 
 int main(){
 
-    ArrayList<Bar*>* list = new ArrayList<Bar*>(10);
-    Bar* bar = new Bar();
-    list->add(bar);
-    //list->clear();
-    delete list;
-
-    std::cout<<"Del";
 }

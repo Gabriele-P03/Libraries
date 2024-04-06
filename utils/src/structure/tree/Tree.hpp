f56e390d56839ed4ff1ce3b7e0c7d42b9fd77289
Gabriele-P03 <pacegabriele@libero.it>
2024-04-06 18:39:22 +0200

    Tree hierarchy

---
+++ utils/src/structure/tree/Tree.hpp
@@ -0,0 +1,22 @@
+/**
+ * It is the implementation of a Generic Tree whose node can have N children
+*/
+
+#ifndef TREE_JPL
+#define TREE_JPL
+
+#include <memory>
+
+namespace jpl{
+    namespace _utils{
+        namespace _collections{
+
+
+            class Tree{
+
+            };
+
+        }
+    }
+}
+#endif
